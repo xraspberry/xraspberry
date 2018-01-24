b0fe11ea8c07f05eb523158eb13dded8553471fc
duguangting <dgt_x@foxmail.com>
2018-01-24 18:02:10 +0800

    db migrations scripts

---
+++ misc/db-migrations/xraspberry/versions/20180124163411_init_db.py
@@ -0,0 +1,24 @@
+"""init db
+
+Revision ID: b675d4882d82
+Revises: 
+Create Date: 2018-01-24 16:34:11.605451
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b675d4882d82'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
